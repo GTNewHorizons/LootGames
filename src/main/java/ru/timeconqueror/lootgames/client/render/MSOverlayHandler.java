a7fce702dc853ff0a631e1d6b339b75699df1a5c
Martin Robertz <dream-master@gmx.net>
2024-07-13 23:20:39 +0700

    Update MSOverlayHandler.java

--- src/main/java/ru/timeconqueror/lootgames/client/render/MSOverlayHandler.java
+++ src/main/java/ru/timeconqueror/lootgames/client/render/MSOverlayHandler.java
@@ -2,7 +2,9 @@ package ru.timeconqueror.lootgames.client.render;
 
 import java.awt.Color;
 import java.lang.ref.WeakReference;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
